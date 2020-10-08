641f634059b794de6dfa8f5ecc7e8855787112d3
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-10-08 18:04:50 +0300

    tabs added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,26 @@ $('.sales__inner').slick({
 });
 
 
+let tabs = document.querySelectorAll('.tabs');
+let contents = document.querySelectorAll('.contents');
+
+let brandsAll = document.querySelector('.brands-all.first');
+let filterItem = document.querySelectorAll('.filter__item');
+
+for (let i = 0; i < filterItem.length; i++) {
+    filterItem[i].onclick = () => brandsAll.hidden = true;
+}
+
+for (let x = 0; x < tabs.length; x++) {
+    for (let i = 0; i < tabs[x].children.length; i++) {
+        tabs[x].children[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            for (let j = 0; j < tabs[x].children.length; j++) {
+                tabs[x].children[j].classList.remove('active');
+                contents[x].children[j].classList.remove('active');
+            }
+            tabs[x].children[i].classList.add('active');
+            contents[x].children[i].classList.add('active');
+        });
+    }
+}
